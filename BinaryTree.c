bc7c319c1a36732025e8e78120bb2eb53c2367d0
AntonioRoldan <antonioroldan95@outlook.com>
2020-10-15 12:31:56 +0100

    C++ binary search tree

--- BinaryTree.c
+++ BinaryTree.c
@@ -1,11 +1,35 @@
-// A 
-//B  C
+/*
+  A 
+B  C
 
-// Preorder ABC
-// Inorder BAC 
-// Postorder BCA 
-// Level order ABC 
+Preorder ABC
+Inorder BAC 
+Postorder BCA 
+Level order ABC 
 
-// Full, complete, strict binary trees 
-// K-D tree 
-// Randomized projection tree 
+Full, complete, strict binary trees 
+K-D tree 
+Randomized projection tree 
+
+Full binary tree, all levels are full, it is always complete 
+Complete binary tree, when traversed in level order, if elements are stored in array there are no blanks in the array
+Strict binary tree, each node has either two or zero children 
+For creating a binary tree a queue is used as well as a cursor pointer p and a temporary pointer t for node creation, here is the procedure: 
+-We create a root node with left and right children pointers set to null 
+-Enqueue its address. 
+-Dequeue address from queue 
+-point p to the dequeued address in this case the root, 
+ -if p has a left child, 
+ -Create node t with the left child's value 
+ -Enqueue t's address 
+ -Point p's left child to t 
+ -if p does not have a left child 
+ -If p has a right child
+ -Create node t with the left child's value 
+ -Enqueue t's address 
+ -Dequeue the next address for the next element and point p to it 
+ -If p does not have a right child 
+ -Dequeue the next address 
+ -Repeat until queue is empty 
+ -We will use a value of -1 to signify a left or right child is empty 
+ */
--- StackLinkedList.c
+++ StackLinkedList.c
@@ -1,8 +1,8 @@
- #include <stdio.h>
- #include <stdlib.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
 struct Node {
- int data;
+ char data;
  struct Node *next;
  }*top=NULL;
 
@@ -40,11 +40,68 @@ void display(){
   }
   printf("\n");
 }
+
+int isBalanced(char *exp)
+{
+  int i;
+  for(i=0;exp[i]!='\0';i++)
+  {
+    if(exp[i]=='(')
+      push(exp[i]);
+    else if(exp[i]==')') {
+      if(top==NULL)
+        return 0;
+      pop();
+    }   
+  }
+  if(top == NULL) return 1; 
+  return 0; 
+}
+int pre(char x) {
+  if(x=='+' || x=='-')
+    return 1;
+  else if(x =='*' || x=='/')
+    return 2;
+  return 0;
+}
+
+int isOperand(char x){
+  if(x=='+' || x=='-' || x=='*' || x=='/')
+    return 0;
+  else
+    return 1;
+}
+  
+char * InToPost(char *infix) {
+  int i=0,j=0;
+  char *postfix;
+  int len=strlen(infix);
+  postfix=(char *)malloc((len+2)*sizeof(char));
+  while(infix[i]!='\0')
+  {
+    if(isOperand(infix[i]))
+      postfix[j++] = infix[i++];
+    else {
+      if(pre(infix[i])>pre(top->data))
+        push(infix[i++]);
+      else {
+        postfix[j++]=pop();
+      }
+    }
+  }
+  while(top!=NULL) postfix[j++]=pop();
+  return postfix;
+ }
+
 int main(int argc, const char * argv[]) {
-  push(10);
-  push(20);
-  push(30);
-  display();
-  printf("%d ",pop());
+  // push(10); // Change type of data to char to run this 
+  // push(20);
+  // push(30);
+  // display();
+  // printf("%d ",pop());
+  char *infix="a+b*c-d/e";
+  push('#');
+  char *postfix=InToPost(infix);
+  printf("%s ",postfix);
   return 0;
 }
