5671b5bd1f38f35a8bdc76caf2da54904cc4c805
AntonioRoldan <antonioroldan95@outlook.com>
2020-10-15 12:43:00 +0100

    Postfix evaluation

--- InfixToPostfixC++.cpp
+++ InfixToPostfixC++.cpp
@@ -74,6 +74,44 @@ char* convert(char* infix){
   return postfix;
  
 }
+
+ 
+int operation(char op, int x, int y){
+  if (op == '+'){
+    return x + y;
+  } else if (op == '-'){
+    return x - y;
+  } else if (op == '*'){
+    return x * y;
+  } else if (op == '/'){
+    return x / y;
+  }
+}
+ 
+
+int postfixEvaluate(char* postfix){
+  stack<int> stk;
+  int x;
+  int y;
+  int result;
+  for (int i=0; postfix[i]!='\0'; i++){
+    if (isOperand(postfix[i])){
+      // int typecast would not work because of char so subtract '0'
+      stk.push(postfix[i]-'0');
+    } else {
+      y = stk.top();
+      stk.pop();
+      x = stk.top();
+      stk.pop();
+      result = operation(postfix[i], x, y);
+      stk.push(result);
+    }
+  }
+  result = stk.top();
+  stk.pop();
+  return result;
+}
+ 
  
  
 int main() {
