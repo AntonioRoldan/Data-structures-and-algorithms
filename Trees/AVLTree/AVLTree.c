10b6ff796a1db5d2e11b09f454e437343a02b45f
AntonioRoldan <antonioroldan95@outlook.com>
2020-10-18 01:07:46 +0100

    AVL trees in c++ and c

--- Trees/AVLTree/AVLTree.c
+++ Trees/AVLTree/AVLTree.c
@@ -25,9 +25,276 @@
   which is the same as the left child of the old root. 
   Its right children are added as the left children of the right child of our new root which is the old root 
   For deletion we have L1 L-1 and L0 rotations depending on the balance factor
-  of the left child of the root with L1 being LL rotation, L-1 being LR rotation. 
+  of the left child of the root with L1 being LL rotation, L-1 being LR rotation.
+  These cases apply when deleting a node from the right side of the root  
   for L0 we can use either of L1 and L-1 same logic applies to R1, R-1 and R0 
+  
+  The following diagram will help understand LR rotation  
+        p                   plr 
+      /   \   ------>    /      \
+     pl                pl        p  
+        \                \     /   
+         plr 
+        /   \
+  
+  pl = p->lChild; 
+  plr = pl->rChild;
+  pl->rChild = plr->lChild; 
+  p->lChild = plr->rChild; 
+
+  plr->lChild = pl; 
+  plr->rChild = p; 
+  Now an example of LL rotation
+        p              pl 
+     /    \    ->    /    \
+    pl                     p 
+   /  \                   /
+      plr               plr
+  
+  pl = p->lChild; 
+  plr = pl->rChild; 
+
+  pl->rChild = p; 
+  p->lChild = plr; 
+  In this case we use the pointer plr to keep track of the soon-to-be root's right child             
+  Whenever we find an imbalance at any node in the tree
+  we always perform the rotation from the third node starting from above
+  If we have a balance factor of -2 since the balance factor is the result
+  of lh - rh we have a right imbalance
+  If the right child of the root whose subtree/tree is imbalanced is -1
+  we have an RR imbalance because the third child is on the right since bf = lh - rh
+  if it is 1 we have RL because the third child is on the left 
+  Same with left imbalances.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct Node {
+  struct Node *lchild;
+  int data;
+  int bf;
+  int height;
+  struct Node *rchild;
+ }*root=NULL;
+
+int height(struct Node *p) {
+  int x=0,y=0;
+  if(!p)return 0;
+  x=height(p->lchild); 
+  y=height(p->rchild);
+  return x>y?x+1:y+1;
+}
+
+void Insert(int key) {
+  struct Node *t=root;
+  struct Node *r=NULL,*p;
+  if(root==NULL) {
+    p=(struct Node *)malloc(sizeof(struct Node));
+    p->data=key;
+    p->bf=0;
+    p->lchild=p->rchild=NULL;
+    root=p;
+    return;
+  }
+
+  while(t!=NULL) {
+    r=t;
+    if(key<t->data)
+      t=t->lchild;
+    else if(key>t->data)
+      t=t->rchild;
+    else
+    return;
+  }
+  p=(struct Node *)malloc(sizeof(struct Node));
+  p->data=key;
+  p->lchild=p->rchild=NULL;
+  if(key<r->data) r->lchild=p;
+  else r->rchild=p;
+}
+struct Node * LLRotation(struct Node *p) {
+  int lbf,rbf;
+  struct Node *pl=p->lchild;
+  pl->bf=0;
+  p->lchild=pl->rchild;
+  pl->rchild=p;
+  lbf=height(p->lchild)+1; rbf=height(p->rchild)+1;
+  p->bf=lbf-rbf;
+  if(p==root)root=pl;
+  return pl;
+}
+struct Node *LRRotation(struct Node *p) {
+  int lbf,rbf;
+  struct Node *pl=p->lchild;
+  struct Node *plr=pl->rchild;
+  plr->bf=0;
+  p->lchild=plr->rchild;
+  pl->rchild=plr->lchild;
+
+  plr->lchild=pl;
+  plr->rchild=p;
+  lbf=height(p->lchild)+1; rbf=height(p->rchild)+1;
+  p->bf=lbf-rbf;
+  lbf=height(pl->lchild)+1; rbf=height(pl->rchild)+1;
+  pl->bf=lbf-rbf;
+  if(p==root)root=plr;
+  return plr;
+}
+struct Node *RRRotation(struct Node *p)
+{
+  int lbf,rbf;
+ struct Node *pr=p->rchild;
+ pr->bf=0;
+ p->rchild=pr->lchild;
+ pr->lchild=p;
+ lbf=height(p->lchild)+1; rbf=height(p->rchild)+1;
+ p->bf=lbf-rbf;
+ if(p==root)root=pr;
+ return pr;
+}
+struct Node *RLRotation(struct Node *p)
+{
+  int lbf,rbf;
+ struct Node *pr=p->rchild;
+ struct Node *prl=pr->lchild;
+ prl->bf=0;
+  p->rchild=prl->lchild;
+ pr->lchild=prl->rchild;
+ prl->rchild=pr;
+ prl->lchild=p;
+ lbf=height(p->lchild)+1; rbf=height(p->rchild)+1;
+ p->bf=lbf-rbf;
+ 
+  lbf=height(pr->lchild)+1; rbf=height(pr->rchild)+1;
+ pr->bf=lbf-rbf;
+ if(p==root)root=prl;
+ return prl;
+}
+
+
+struct Node* RInsert(struct Node *p,int key)
+{
+  struct Node *t;
+  int lbf,rbf;
+  if(p==NULL) {
+    t=(struct Node *)malloc(sizeof(struct Node));
+    t->data=key;
+    t->bf=0;
+    t->lchild=t->rchild=NULL;
+    return t;
+  }
+  if(key<p->data)
+    p->lchild=RInsert(p->lchild,key);
+  else if(key>p->data)
+    p->rchild=RInsert(p->rchild,key);
+  lbf=height(p->lchild)+1; rbf=height(p->rchild)+1;
+  p->bf=lbf-rbf;
+  if(p->bf==2 && p->lchild->bf==1)
+    return LLRotation(p);
+  if(p->bf==2 && p->lchild->bf==-1)
+    return LRRotation(p);
+  if(p->bf==-2 && p->rchild->bf==-1)
+    return RRRotation(p);
+  if(p->bf==-2 && p->rchild->bf==1)
+    return RLRotation(p);
+  return p;
+}
+void Inorder(struct Node *p){
+  if(p){
+    Inorder(p->lchild);
+    printf("%d ",p->data);
+    Inorder(p->rchild);
+  }
+}
+struct Node * Search(int key){
+  struct Node *t=root;
+  while(t!=NULL){
+    if(key==t->data)
+      return t;
+    else if(key<t->data)
+      t=t->lchild;
+    else
+      t=t->rchild;
+  }
+  return NULL;
+}
+
+struct Node *InSucc(struct Node *p) {
+  while(p && p->lchild!=NULL)
+    p=p->lchild;
+  return p;
+}
+
+struct Node *InPre(struct Node *p) {
+  while(p && p->rchild!=NULL)
+    p=p->rchild;
+  return p;
+}
+
+struct Node* Delete(struct Node *p, int key) {
+  struct Node *q; 
+
+  if(p == NULL) 
+    return NULL;
+  if(p->lchild == NULL && p->rchild == NULL){
+    if(p==root)
+      root=NULL;
+    free(p);
+    return NULL;
+  }
+  if(key < p->data)
+    p->lchild = Delete(p->lchild, key);
+  else if(key > p->data)
+    p->rchild = Delete(p->rchild, key);
+  else {
+    if((p->lchild && p->lchild->height) > (p->rchild && p->rchild->height)) {
+      q = InPre(p->lchild);
+      p->data = q->data;
+      p->lchild = Delete(p->lchild, p->data);
+    } else {
+      q = InSucc(p->rchild);
+      p->data = q->data;
+      p->rchild = Delete(p->rchild, p->data);
+    }
+  }
+  p->height = height(p);
+
+  if(p->bf == 2) {
+    if(p->lchild->bf == 1) 
+      return LLRotation(p);
+    else 
+      return LRRotation(p);
+  }
+  if(p->bf == -2) {
+    if(p->rchild->bf == -1) 
+      return RRRotation(p);
+    else 
+      return RLRotation(p);
+  }
+  return p;
+}
+int main() {
+  struct Node *temp;
+  Insert(30);
+  RInsert(root,50);
+  RInsert(root,40);
+  RInsert(root,20);
+  RInsert(root,10);
+  RInsert(root,42);
+  RInsert(root,46);
+  Delete(root,20);
+  Inorder(root);
+  printf("\n");
+
+  temp=Search(2);
+  if(temp!=NULL)
+    printf("element %d is found\n",temp->data);
+  else
+    printf("element is not found\n");
+  return 0;
+}
 
 
 
